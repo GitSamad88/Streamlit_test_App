522b393b13769d18a06ad9ae0dbd5daab3c19b1d
ABDESSAMAD TAOUFQ <110288424+GitSamad88@users.noreply.github.com>
2023-11-30 18:40:51 +0100

    Update 1-test_page1.py

--- pages/1-test_page1.py
+++ pages/1-test_page1.py
@@ -2,10 +2,18 @@ import streamlit as st
 import datetime as dt
 import pandas as pd
 
-if "emplois" not in st.session_state:
+if "emplois1" not in st.session_state:
     st.session_state["emplois"]=pd.DataFrame()
-example = pd.read_csv(
+example1 = pd.read_csv(
             "https://docs.google.com/spreadsheets/d/1tWMj4Lfe22t2aUuLiromCUhAN5bsXA4OFPI_sXz6ELQ/gviz/tq?tqx=out:csv&sheet=emplois_3_4")
 st.info("Emplois du temps de 3 et 4 aep: ")
-st.table(example)
-st.session_state["emplois"]=example
+st.table(example1)
+st.session_state["emplois1"]=example1
+
+
+if "emplois2" not in st.session_state:
+    st.session_state["emplois2"]=pd.DataFrame()
+example2 = pd.read_csv(r"C:\Users\hp\Downloads\2_emplois_3_4.csv")
+st.info("Emplois du temps de 3 et 4 aep: ")
+st.table(example2)
+st.session_state["emplois2"]=example2
